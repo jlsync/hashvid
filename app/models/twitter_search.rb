35964b3cf9b3c6c9e8846a810798c788af2802f8
Jason Lee <jlsync@gmail.com>
2009-05-10 17:36:59 +0100

    twitter search by date

--- app/models/twitter_search.rb
+++ app/models/twitter_search.rb
@@ -1,12 +1,25 @@
 class TwitterSearch
 
-  def self.search(text)
+  class Twitter::Search
+    def until_date(date)
+      @query[:until] = date.is_a?(String) ? date : date.strftime("%Y-%m-%d")
+      self
+    end
 
-    search = Twitter::Search.new(text)
-     
-    puts '*'*50, 'First Run', '*'*50
-    search.each { |result| puts result.inspect }
+    def since_date(date)
+      @query[:since] = date.is_a?(String) ? date : date.strftime("%Y-%m-%d")
+      self
+    end
+
   end
 
+
+  def self.search(text, from , to  )
+    search = Twitter::Search.new(text).since_date(from).until_date(to)
+     
+    #puts '*'*50, 'First Run', '*'*50
+    #search.each { |result| puts result.inspect }
+    search.fetch
+  end
 
 end
